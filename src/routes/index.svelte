c3d845ef72b1733cc4ce7f36e61d007c90a1db88
Liam McArdle <mcardle.liam@gmail.com>
2021-09-28 21:52:43 -0700

    base calendar

---
+++ src/routes/index.svelte
@@ -0,0 +1,20 @@
+<script>
+	import { onMount } from 'svelte';
+	import { browser } from '$app/env';
+	import { Calendar } from '@fullcalendar/core';
+	import dayGridPlugin from '@fullcalendar/daygrid';
+
+	let calendarEl;
+	let calendar;
+
+	onMount(async () => {
+		await import('@fullcalendar/core/vdom');
+		calendar = new Calendar(calendarEl, {
+			plugins: [dayGridPlugin],
+			initialView: 'dayGridMonth'
+		});
+		calendar.render();
+	});
+</script>
+
+<div bind:this={calendarEl} />
